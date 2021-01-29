e0131949347f4fb7b589672832ca7ea9efcde41f
Pimenta <jose.p.oliveira@itsector.pt>
2021-01-29 17:22:15 +0000

    *Change: added close image in pokemon detail modal view controller

--- PokemonDemo/Controllers/PokemonDetailViewController.swift
+++ PokemonDemo/Controllers/PokemonDetailViewController.swift
@@ -11,21 +11,40 @@ class PokemonDetailViewController: UIViewController {
     
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var scrollViewHeight: NSLayoutConstraint!
-    @IBOutlet var pokemonImageView: UIImageView!
-    @IBOutlet var pokemonIdLabel: UILabel!
-    @IBOutlet var pokemonNameLabel: UILabel!
-    @IBOutlet var pokemonDescriptionTitle: UILabel!
-    @IBOutlet var pokemonDescriptionText: UILabel!
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var idLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var typesTitleLabel: UILabel!
+    @IBOutlet var typesTextLabel: UILabel!
     
+    public var model: PokemonCellViewModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+    
+    func setupUI() {
+        
+        //Set the UI with the pokemon information
+        imgView.image = model?.pokemonImage
+        idLabel.text = ("#\(model?.pokemonId ?? "0")")
+        nameLabel.text = model?.pokemonName
+        
+        var typesString: [String] = []
+        for types in model!.pokemonTypes {
+            typesString.append(types.type.name.capitalized)
+        }
+        typesTextLabel.text = typesString.joined(separator: ", ")
     }
     
     override func viewDidLayoutSubviews() {
         updateScrollView()
     }
     
+    @IBAction func closeButtonClicked(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
 
 extension PokemonDetailViewController: UIScrollViewDelegate {
